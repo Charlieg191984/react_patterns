e63ca82049a3d747705cf24a6a570c85767b8685
charlie <cgiraldo1919@gmail.com>
2023-01-17 12:33:10 -0500

    created new component components/TypeWriter/TypeWriter.jsx

---
+++ src/components/TypeWriter/TypeWriter.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const TypeWriter = () => {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis eligendi voluptates ipsa molestiae cum, dolore incidunt illo corporis, necessitatibus doloribus, deserunt ratione exercitationem quibusdam animi harum tempore dolorem est natus laboriosam obcaecati veniam iusto autem excepturi esse. Alias aperiam nesciunt veniam autem culpa vero. Ullam temporibus beatae maxime consequatur minus.
+      </h1>
+      <h2>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias odio tenetur dicta voluptatum dolore commodi odit, a cum vero nisi veritatis quibusdam harum, inventore at facilis ab eius optio vel laborum laudantium quidem cumque. Inventore, amet? Perferendis odit reprehenderit iusto?
+      </h2>
+      <h3>
+       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error laborum possimus labore voluptate assumenda repellat provident? Voluptatem quod eum optio atque, ad asperiores debitis ratione ex officia magni. Voluptatum, recusandae.
+      </h3>
+      <h4>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus neque odit eligendi repellat voluptas consequatur sapiente provident suscipit maxime architecto.
+      </h4>
+    </div>
+  )
+}
+
+export default TypeWriter
